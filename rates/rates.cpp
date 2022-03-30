520d0b072ee79fa42937074ba78338717e3a4496
Ivan <cmd7889@gmail.com>
2022-03-30 21:10:23 +0300

    add sleep

--- rates/rates.cpp
+++ rates/rates.cpp
@@ -1,5 +1,6 @@
 ﻿#define CURL_STATICLIB
 #include <iostream>
+#include <windows.h>
 #include <curl\curl.h>
 #include <nlohmann\json.hpp>
 
@@ -13,28 +14,31 @@ static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* use
 int main()
 {
 
-    std::string readBuffer;
-    CURL* curl = curl_easy_init();
 
-    curl_easy_setopt(curl, CURLOPT_URL, "https://www.cbr-xml-daily.ru/daily_json.js"); // задаем url на нужный json
-    curl_easy_setopt(curl, CURLOPT_POST, 0); // выключаем POST
-    curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);// включаем GET
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback); // функция вызываемая для записи полученных данных в буфер
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer); // указатель на буфер с данными куда их писать
+    
+    while (1) {
 
-    CURLcode res = curl_easy_perform(curl); //  Выполняем запрос
-    curl_easy_cleanup(curl); // очистка
+        std::string readBuffer;
+        CURL* curl;
 
+        curl = curl_easy_init();
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.cbr-xml-daily.ru/daily_json.js"); // задаем url на нужный json
+        curl_easy_setopt(curl, CURLOPT_POST, 0); // выключаем POST
+        curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);// включаем GET
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback); // функция вызываемая для записи полученных данных в буфер
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer); // указатель на буфер с данными куда их писать
 
-    std::cout << res << std::endl; // выводит код ошибки, если 0, то все ок
+        CURLcode res = curl_easy_perform(curl); //  Выполняем запрос
+        curl_easy_cleanup(curl); // очистка
 
-    if (res == 0) { // проверяем код ошибки, если 0, то все ок
+        if (res == 0) { // проверяем код ошибки, если 0, то все ок
+            auto jdata = nlohmann::json::parse(readBuffer); // преобразуем полученную JSON строку в объект 
+            std::cout << jdata << std::endl;
+        }
 
-        auto jdata = nlohmann::json::parse(readBuffer); // преобразуем полученную JSON строку в объект 
-        std::cout << jdata << std::endl;
+        Sleep(1000);
+
     }
-
- 
     
     return 0;
 }
